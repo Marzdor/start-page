2326ca334ecc5eab915dbe2f25e10569a1cfc74a
Marzdor <marzdor@gmail.com>
2018-02-12 19:59:03 -0800

    Now there is a build of site to test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var path = require("path");
 module.exports = {
   entry: {
     App: "./app/assets/scripts/App.js",
-    Vendor: "./app/assets/scripts/Vendor.js"
   },
   output: {
     path: path.resolve(__dirname, "./app/temp/scripts"),
